83d971f594e6a5f505ae5211a70208f24d448c18
Geraldo Silva <gmagela@kenzan.com>
2018-07-21 19:03:57 -0400

    Remove the SpringBootTest annotation from the unit tests

--- service/src/test/java/com/springcloud/service/ServiceApplicationTests.java
+++ service/src/test/java/com/springcloud/service/ServiceApplicationTests.java
@@ -2,15 +2,14 @@ package com.springcloud.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+
+    }
 
 }
